99e86a906efe402e7e1a71266f5ac06b012dfba1
Lesley Labady <labadylesley@gmail.com>
2022-07-24 14:18:01 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const m = document.getElementById("main")
+m.remove()
+const newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+newHeader.innerHTML = "Lesley Labady is the champion"
